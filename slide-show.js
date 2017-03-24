5ebd9c53d9ef631b6d94e1b0d19f67de389bf18b
Ken Kahn <toontalk@gmail.com>
2017-03-24 15:28:10 +0800

    A simple slide show with iframes

---
+++ slide-show.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var slides = document.querySelectorAll('.slide');
+    var current_slide = 0;
+    var increment_slide = function (increment) {
+        slides[current_slide].className = 'slide';
+        slides[current_slide].style.opacity = 0;
+        slides[current_slide].classList.remove('showing');
+        current_slide = (current_slide+increment)%slides.length;
+        slides[current_slide].classList.add('showing');
+        slides[current_slide].style.opacity = 1;
+    };
+    var create_button = function (label) {
+        var button = document.createElement('button');
+        button.className = "slide-show-button";
+        button.innerHTML = label;
+        return button;
+    };
+    var next     = create_button('&#9654;');
+    var previous = create_button('&#9664;');
+    document.body.insertBefore(previous, document.body.firstChild);
+    document.body.insertBefore(next,     document.body.firstChild);
+    next    .addEventListener('click', function () {
+        increment_slide(1);
+    });
+    previous.addEventListener('click', function () {
+        increment_slide(-1);
+    });
+    increment_slide(0);
+});
