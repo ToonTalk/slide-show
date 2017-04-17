9eb607ff99859d86319fe575940fda358c5ef0a3
Ken Kahn <toontalk@gmail.com>
2017-04-17 16:34:25 +0800

    Posts messages to iframes as visibility changes

--- slide-show.js
+++ slide-show.js
@@ -2,11 +2,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var slides = document.querySelectorAll('.slide');
     var current_slide = 0;
     var increment_slide = function (increment) {
+        var iframes, i;
         slides[current_slide].className = 'slide';
 //         slides[current_slide].style.opacity = 0;
         slides[current_slide].classList.remove('showing');
+        iframes = slides[current_slide].getElementsByTagName('iframe');
+        for (i = 0; i < iframes.length; i++) {
+            iframes[i].contentWindow.postMessage("hidden", "*");
+        };
         current_slide = (current_slide+increment)%slides.length;
         slides[current_slide].classList.add('showing');
+        iframes = slides[current_slide].getElementsByTagName('iframe');
+        for (i = 0; i < iframes.length; i++) {
+            iframes[i].contentWindow.postMessage("shown", "*");
+        };
         previous.disabled = (current_slide === 0);
         next.disabled     = (current_slide === slides.length-1);
 //         slides[current_slide].style.opacity = 1;
